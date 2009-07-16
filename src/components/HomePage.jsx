00e669216dc49405232a985d5ad88f5bc9798c66
unknown <jonen54@gmail.com>
2009-07-17 00:47:19 +0300

    [CHORE] Added form flipper

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -13,8 +13,27 @@ class Carousel extends React.Component{
 		      						<img src={`images/${image}.jpg`}/>
 		    					</div>)
 							}
-						)
-					}
+						)}
+				</div>
+			)}
+}
+
+class Note extends React.Component{
+	render(){
+		return(
+				<p className="note translucent center white col s12">
+						{this.props.children}
+				</p>
+			)}
+}
+
+class Fliper extends React.Component{
+	render(){
+		return(
+				<div className="col s12 center">
+					<button className="btn flip_me">
+						{this.props.children}
+					</button>
 				</div>
 			)
 	}
@@ -35,10 +54,24 @@ class HomeScreen extends React.Component{
 						<div className="flipper">
 							<div id="loginForm_area">
 								<LoginForm />
+								<Note>
+									Don't have an account?
+								</Note>
+
+								<Fliper>
+									Register
+								</Fliper>
 							</div>
 
 							<div id="registerForm_area">
 								<RegisterForm />
+								<Note>
+									Already have an account?
+								</Note>
+
+								<Fliper>
+									Login
+								</Fliper>
 							</div>
 
 						</div>
