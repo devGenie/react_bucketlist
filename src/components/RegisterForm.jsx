af7d6622545586d801755960628666ab39ac3448
unknown <jonen54@gmail.com>
2009-07-17 00:45:25 +0300

    [CHORE] bound Onchange to form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,37 +4,56 @@ import FieldWrapper from './FieldWrapper';
 class RegisterForm extends React.Component{
 	constructor(props){
 		super(props);
+
+		this.state={
+			'email':'',
+			'first_name':'',
+			'last_name':'',
+			'password1':'',
+			'password2':''
+		}
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleSubmit(event){
 		event.preventDefault();
-	}	
+	}
+
+	handleChange(event){
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]:value
+		})
+	}
 	render(){
 			return(
-					<form onSubmit={this.handleSubmit}>
-						<FieldWrapper>
-							<input type="text" name="first_name"/>
+					<form className="col s12 translucent" onSubmit={this.handleSubmit}>
+						<FieldWrapper Label='Email'>
+							<input type="text" name="email" onChange={this.handleChange} />
 						</FieldWrapper>
 
-						<FieldWrapper>
-							<input type="text" name="last_name" />
+						<FieldWrapper Label='First Name'>
+							<input type="text" name="first_name" onChange={this.handleChange}/>
 						</FieldWrapper>
 
-						<FieldWrapper>
-							<input type="text" name="email" />
+						<FieldWrapper Label='Last Name'>
+							<input type="text" name="last_name" onChange={this.handleChange}/>
 						</FieldWrapper>
 
-						<FieldWrapper>
-							<input type="password" name="password1" />
+						<FieldWrapper Label='Password'>
+							<input type="password" name="password1" onChange={this.handleChange}/>
 						</FieldWrapper>
 
-						<FieldWrapper>
-							<input type="password" name="password2" />
+						<FieldWrapper Label='Repeat password'>
+							<input type="password" name="password2" onChange={this.handleChange}/>
 						</FieldWrapper>
 
-						<FieldWrapper>
-							<button type="submit" name="submit">Register</button>
+						<FieldWrapper extraz="center">
+							<button type="submit" name="submit" className='btn'>Register</button>
 						</FieldWrapper>
 					</form>
 			)}
