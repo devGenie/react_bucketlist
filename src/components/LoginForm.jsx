f44baaae1a3833da4e1f22a8a236ffd406e30f1d
unknown <jonen54@gmail.com>
2009-07-17 00:46:23 +0300

    [FEATUE] Post to login endpoint

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import FieldWrapper from './FieldWrapper'
+import FieldWrapper from './FieldWrapper';
 
 class LoginForm extends React.Component {
 	constructor(props){
 		super(props);
+
+		this.state = {
+			email : '',
+			password: ''
+		}
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleSubmit(event){
 		event.preventDefault();
+		fetch("https://bucketapi.herokuapp.com/api/v1/auth/login",
+		      {headers:{
+						'Content-Type':'application/json'
+						}, 
+			    method:'POST',
+				body:JSON.stringify({'email':this.state.email,'password':this.state.password})})
+					 .then((response) => response.json())
+					 .then((jsonResponse) => {
+					 	console.log(JSON.stringify(jsonResponse));
+					 	if (jsonResponse.status=='success'){
+					 		sessionStorage.setItem('auth',jsonResponse.auth)
+					 	}
+					 	alert(sessionStorage.getItem('auth'))
+					 })
+	}
+
+	handleChange(event){
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]:value
+		});
 	}
 	render(){
 		return (<form className="col s12 translucent" onSubmit={this.handleSubmit}>
@@ -16,11 +46,11 @@ class LoginForm extends React.Component {
 							Login
 					</h5>
 					<FieldWrapper Label="Email">
-						<input type="text" id="login_username" />
+						<input type="text" name='email' id="login_username" onChange={this.handleChange}/>
 					</FieldWrapper>
 
 					<FieldWrapper Label="Password">
-						<input type="password" id="login_password" />
+						<input type="password" id="login_password" name='password' onChange={this.handleChange}/>
 					</FieldWrapper>
 
 					<FieldWrapper extraz="center">
