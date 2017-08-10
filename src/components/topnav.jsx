779c726739b8d0a774cfd75622b86dd6839855f3
unknown <jonen54@gmail.com>
2017-08-10 15:41:49 +0300

    Added top navigation bar component

---
+++ src/components/topnav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Topnav extends React.Component{
+	render(){
+		return(
+				<div className="navbar-fixed" id="mainNav">
+						<nav id="home_nav">
+							<div className="nav-wrapper">
+								<div className="container">
+									<a href="#" className="brand-logo">BucketlistFairy</a>
+									<ul id="topNave" className="right hide-on-med-and-down">
+										{this.props.children}
+									</ul>
+								</div>
+							</div>
+						</nav>
+					</div>
+			)
+	}
+}
+
+export default Topnav;
