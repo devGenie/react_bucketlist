485886776af53b785b88f4e458274f69b85da6f2
unknown <jonen54@gmail.com>
2017-08-10 12:06:20 +0300

    Added routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import HomePage from './components/HomePage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App =()=>(
+  <Router>
+      <Route path="/" component={HomePage}/>
+  </Router>
+  );
 
-export default App;
+export default App; 
