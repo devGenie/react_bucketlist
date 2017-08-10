3a4c0fc20599ece7c03edc899db08255fe8962fb
unknown <jonen54@gmail.com>
2017-08-10 15:30:33 +0300

    Added wrapper for input fields

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FieldWrapper from './FieldWrapper'
 
 class LoginForm extends React.Component {
 	constructor(props){
@@ -10,10 +11,21 @@ class LoginForm extends React.Component {
 		event.preventDefault();
 	}
 	render(){
-		return (<form onSubmit={this.handleSubmit}>
-					<input type="text" id="login_username"/>
-					<input type="password" id="login_password"/>
-					<button type="submit" id="loginBtn">Login</button>
+		return (<form className="col s12 translucent" onSubmit={this.handleSubmit}>
+					<h5 className="col s12 center">
+							Login
+					</h5>
+					<FieldWrapper Label="Email">
+						<input type="text" id="login_username" />
+					</FieldWrapper>
+
+					<FieldWrapper Label="Password">
+						<input type="password" id="login_password" />
+					</FieldWrapper>
+
+					<FieldWrapper extraz="center">
+						<button type="submit" id="loginBtn" className="btn">Login</button>
+					</FieldWrapper>
 				</form>)
 	}
 }
