bdc44279041d3d927657631340a929771c3913ad
unknown <jonen54@gmail.com>
2017-08-11 08:20:14 +0300

    Rendering bucketlists on load

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Topnav from './topnav'
+import Topnav from './topnav';
+import BucketList from './bucketlist';
+import BucketListForm from './bucketlist-form';
 
 class AddButton extends React.Component{
 	render(){
@@ -11,7 +13,52 @@ class AddButton extends React.Component{
 	}
 }
 
+class BucketLists extends React.Component{
+	constructor(props){
+		super(props)
+	}
+
+	componentDidUpdate(){
+		console.log(this.props.data)
+	}
+
+	render(){
+		return(
+				<div className='row'>
+					{this.props.data.map((dataPoint)=>{
+						return <BucketList data={dataPoint}/>
+					})}
+				</div>
+			)
+	}
+}
+
 class DashBoard extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={bucketlists:[]}
+	}
+
+	componentDidMount(){
+		fetch("https://bucketapi.herokuapp.com/api/v1/bucketlists/",
+			   {headers:{
+			   		Authorization:sessionStorage.getItem('auth')
+			   },
+			   "method":"GET"}
+		).then((response)=>response.json())
+		.then((jsonResponse)=>{
+			var res=JSON.stringify(jsonResponse);
+			if(jsonResponse.status=='success'){
+				this.setState({
+					bucketlists:jsonResponse.data
+				});
+			}
+			else{
+				alert(jsonResponse.message);
+			}
+		})
+	}
+
 	render(){
 		return(
 				<div>
@@ -20,7 +67,10 @@ class DashBoard extends React.Component{
 						<div className="row" id="bklist_controller">
 							<AddButton/>
 						</div>
+
+						<BucketLists data={this.state.bucketlists}/>
 					</div>
+					<BucketListForm/>
 				</div>
 			)}
 }
