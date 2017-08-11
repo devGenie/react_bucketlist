eedb4bf28f24259c24de245c4df0348d78c88842
unknown <jonen54@gmail.com>
2017-08-11 08:17:54 +0300

    Added event listeners on change

--- src/components/bucketlist-form.jsx
+++ src/components/bucketlist-form.jsx
@@ -2,20 +2,43 @@ import React from 'react';
 import FieldWrapper from './FieldWrapper';
 
 class BucketlistForm extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			name:'',
+			description:''
+		}
+		this.handleChange=this.handleChange.bind(this);
+		this.handleSubmit=this.handleSubmit.bind(this);
+	}
+
+	handleChange(event){
+		var target=event.target;
+		var name=target.name;
+		var value=target.value;
+
+		this.setState({
+			[name]:value
+		})
+	}
+
+	handleSubmit(event){
+		event.preventDefault()
+	}
 	render(){
 		return(
-			<form className="modal" id="activityModal">
+			<form className="modal" id="activityModal" onSubmit={this.handleSubmit}>
 				<h4 className="modal-header center">
-					Add Activity
+					New Bucketlist
 				</h4>
 
 				<div className="modal-content">
-					<FieldWrapper Label="email">
-						<input type="text" name="name" id="name" className="validate" />			
+					<FieldWrapper Label="Name">
+						<input type="text" name="name" id="name" className="validate" onChange={this.handleChange}/>			
 					</FieldWrapper>
 
 					<FieldWrapper Label="story">
-						<textarea name="story"></textarea>
+						<textarea name="description" onChange={this.handleChange}></textarea>
 					</FieldWrapper>
 
 					<FieldWrapper extraz="center">
