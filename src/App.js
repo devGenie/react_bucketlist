bd983f4f9fd74c5daeb49d733eee0dea118acad8
unknown <jonen54@gmail.com>
2017-08-11 05:17:49 +0300

    Added URL redirection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage';
+import DashBoard from './components/dashboard';
 
 const App =()=>(
-  <Router>
-      <Route path="/" component={HomePage}/>
-  </Router>
+	  <Router>
+	  	<div>
+	  		<Switch>
+		      <Route exact path="/dashboard" component={DashBoard}/>
+		      <Route path="/" component={HomePage}/>
+		    </Switch>
+	    </div>
+	  </Router>
   );
 
 export default App; 
