dec59cfcf32425109980cb93446be80e756ef238
devgenie <oyomthojohnpaul@gmail.com>
2017-08-13 06:28:46 +0300

    [FEATURE] Complete bucketlist item

--- src/components/bucketlist-item.jsx
+++ src/components/bucketlist-item.jsx
@@ -34,19 +34,37 @@ class BucketListItem extends React.Component{
 	}
 
 	handleDone(){
-
+		var url ="https://bucketapi.herokuapp.com/api/v1/bucketlists/";
+		var itemsUrl=url+this.props.bucketlist+"/items/"+this.props.data.id+"/complete";
+		fetch(itemsUrl,
+			   {headers:{
+			   		Authorization:sessionStorage.getItem('auth')
+			   },
+			   "method":"PUT"}
+		).then((response)=>response.json())
+		.then((jsonResponse)=>{
+			console.log(jsonResponse)
+			if(jsonResponse.status=='success'){	
+				console.log("yess")
+			}
+			else{
+				console.log(jsonResponse.message);
+			}
+		})
 	}
 	render(){
 		return(
-				<li className="collection-item">
-					<div>
-				      <span className="title">{this.props.data.name}</span>
+				<li className="items">
+				      <span className="item_name">{this.props.data.name}</span>
 					  <div className="right">
 				      	<a className="secondary-content" onClick={this.handleDelete}><i className="material-icons">delete</i></a>
-				      	<a className="secondary-content"><i className="material-icons">edit</i></a>
-				      	<a sclassName="secondary-content"><i className="material-icons">done</i></a>
+				      	<a className="secondary-content" ><i className="material-icons">edit</i></a>
+				      	<a sclassName="secondary-content" onClick={this.handleDone}><i className="material-icons">done</i></a>
 					  </div>
-				     </div>
+					  <span className="complete_status">
+						  {this.props.data.complete_status==true? <div className="left">{this.props.data.date_completed}</div>:<div>{"Not completed"}</div>
+						  }
+					  </span>
 				   </li>
 			)
 	}
