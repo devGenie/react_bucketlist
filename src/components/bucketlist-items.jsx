c2e7f3dc02d97ec58ad662e9239d1a2bfcb1235b
andela13 <jonen54@gmail.com>
2017-08-18 18:13:14 +0300

    Pass selected item back to parent component

--- src/components/bucketlist-items.jsx
+++ src/components/bucketlist-items.jsx
@@ -4,15 +4,20 @@ import BucketListItem from './bucketlist-item';
 class BucketListItems extends React.Component{
 	constructor(props){
 		super(props)
+		this.handleItemEdit=this.handleItemEdit.bind(this)
 	}
 
 	componentWillReceiveProps(pp){
+	}
+
+	handleItemEdit(item_id){
+		this.props.editFunc(item_id)
 	}
 	render(){
 		return(
 				<ul className="bkitems">
 					{this.props.data.map((item,index)=>{
-						return <BucketListItem data={item} bucketlist={this.props.bucketlist} itemIndex={index} onDelete={this.props.deleteFunc}/> 
+						return <BucketListItem data={item} bucketlist={this.props.bucketlist} itemIndex={index} onEdit={this.props.editFunc} onDelete={this.props.deleteFunc}/> 
 					})}
 				</ul>
 			)
