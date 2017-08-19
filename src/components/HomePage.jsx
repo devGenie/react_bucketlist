98ebf9980713fd8a51dd887841e6568d228d4129
andela13 <jonen54@gmail.com>
2017-08-19 18:19:00 +0300

    Moved notification to individual forms

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,20 +3,6 @@ import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import Topnav from './topnav';
 
-class Carousel extends React.Component{
-	render(){
-		return(
-				<div className="carousel carousel-slider center">
-					{this.props.imageUrl.map((image) => {
-						console.log(image)
-						return (<div className="carousel-item genie-carousel-item" href="#one!">
-		      						<img src={`images/${image}.jpg`}/>
-		    					</div>)
-							}
-						)}
-				</div>
-			)}
-}
 
 class Note extends React.Component{
 	render(){
@@ -47,6 +33,7 @@ class HomeScreen extends React.Component{
 	render(){
 		return (
 				<div className="genie-carousel">
+					
 					<div id="forms" className="row">
 						<div className="flipper">
 							<div id="loginForm_area">
@@ -74,7 +61,6 @@ class HomeScreen extends React.Component{
 						</div>
 					</div>
 
-					<Carousel imageUrl = {this.imageUrls} />
 				</div>
 				);
 			}
