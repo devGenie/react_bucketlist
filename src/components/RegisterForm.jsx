b730eaea9fc1c83e0cb1a2c569e79d776a94c6bc
andela13 <jonen54@gmail.com>
2017-08-19 18:36:28 +0300

    Added notifications onsubmit form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import FieldWrapper from './FieldWrapper';
+import Loading from './notifications/loading';
+
 
 class RegisterForm extends React.Component{
 	constructor(props){
@@ -10,7 +12,9 @@ class RegisterForm extends React.Component{
 			'first_name':'',
 			'last_name':'',
 			'password1':'',
-			'password2':''
+			'password2':'',
+			'progress':'hide',
+			'button':''
 		}
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
@@ -18,6 +22,8 @@ class RegisterForm extends React.Component{
 
 	handleSubmit(event){
 		event.preventDefault();
+		event.preventDefault();
+		this.setState({progress:'show',button:'disabled'})
 	}
 
 	handleChange(event){
@@ -32,29 +38,36 @@ class RegisterForm extends React.Component{
 	render(){
 			return(
 					<form className="col s12 translucent" onSubmit={this.handleSubmit}>
-						<FieldWrapper Label='Email'>
-							<input type="text" name="email" onChange={this.handleChange} />
-						</FieldWrapper>
+						<Loading status={this.state.progress}/>
+						<div className="innerForm col s12">
+							<h5 className="col s12 center">
+								Register
+							</h5>
 
-						<FieldWrapper Label='First Name'>
-							<input type="text" name="first_name" onChange={this.handleChange}/>
-						</FieldWrapper>
+							<FieldWrapper Label='Email'>
+								<input type="text" name="email" onChange={this.handleChange} />
+							</FieldWrapper>
 
-						<FieldWrapper Label='Last Name'>
-							<input type="text" name="last_name" onChange={this.handleChange}/>
-						</FieldWrapper>
+							<FieldWrapper Label='First Name'>
+								<input type="text" name="first_name" onChange={this.handleChange}/>
+							</FieldWrapper>
 
-						<FieldWrapper Label='Password'>
-							<input type="password" name="password1" onChange={this.handleChange}/>
-						</FieldWrapper>
+							<FieldWrapper Label='Last Name'>
+								<input type="text" name="last_name" onChange={this.handleChange}/>
+							</FieldWrapper>
 
-						<FieldWrapper Label='Repeat password'>
-							<input type="password" name="password2" onChange={this.handleChange}/>
-						</FieldWrapper>
+							<FieldWrapper Label='Password'>
+								<input type="password" name="password1" onChange={this.handleChange}/>
+							</FieldWrapper>
 
-						<FieldWrapper extraz="center">
-							<button type="submit" name="submit" className='btn'>Register</button>
-						</FieldWrapper>
+							<FieldWrapper Label='Repeat password'>
+								<input type="password" name="password2" onChange={this.handleChange}/>
+							</FieldWrapper>
+
+							<FieldWrapper extraz="center">
+								<button type="submit" name="submit" className={'btn '+this.state.button}>Register</button>
+							</FieldWrapper>
+						</div>
 					</form>
 			)}
 	}
