6f7879f2033508bf4917864089173f9899c3d259
andela13 <jonen54@gmail.com>
2017-08-20 05:41:07 +0300

    Wrapping form input elements

--- src/components/FieldWrapper.jsx
+++ src/components/FieldWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class FieldWrapper extends React.Component{
 	render(){
-		return (<div className={`input-field col s12 ${this.props.extraz? this.props.extraz:" "}`}>
+		return (<div className={`input-field ${this.props.extraz? this.props.extraz:" "}`}>
 					{this.props.children}
 					<label>{this.props.Label}</label>
 				</div>
