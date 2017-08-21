af401f3b36506ff45688dfeb84e32f416e1e1fff
andela13 <jonen54@gmail.com>
2017-08-21 19:26:56 +0300

    Picking search term

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,12 +4,20 @@ class Search extends React.Component{
 	constructor(props){
 		super(props)
 		this.state={search_term:''}
+		this.search=this.search.bind(this)
+	}
+
+	search(event){
+		event.preventDefault()
+		let searchTerm=event.target.value
+
+		console.log(searchTerm)
 	}
 
 	render(){
 		return(
 			<div className="searchbar card">
-            		<input placeholder='Search' id="search"/><i className="search-icon material-icons">search</i>
+            		<input placeholder='Search' onChange={this.search} id="search"/><i className="search-icon material-icons">search</i>
           	</div>
 			)
 	}
