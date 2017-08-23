c6a8d7a3b0d7ae0a789f7420a4897eef2686931d
andela13 <jonen54@gmail.com>
2017-08-23 16:17:01 +0300

    List search results

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,42 +1,74 @@
 import React from 'react';
 
+
+class SearchResults extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={results:[]}
+	}
+
+	componentWillReceiveProps(results){
+		this.setState({
+			results:results.results
+		})
+	}
+
+	render(){
+		return(
+				<div className='search-results'>
+					{this.state.results.map((result,index)=>{
+						return(
+								<div className='search-result'>
+									{result.name}
+								</div>
+							)
+						}
+					)}
+				</div>
+
+			)
+	}
+}
+
 class Search extends React.Component{
 	constructor(props){
 		super(props)
-		this.state={search_term:''}
+		this.state={search_term:'',results:[]}
 		this.search=this.search.bind(this)
 	}
 
 	search(event){
 		event.preventDefault()
 		let search_term=event.target.value;
-		fetch("https://bucketapi.herokuapp.com/api/v1/bucketlists/?search="+search_term,
-			   {headers:{
-			   		Authorization:sessionStorage.getItem('auth')
-			   },
-			   "method":"GET"}
-		).then((response)=>response.json())
-		.then((jsonResponse)=>{
-			let res=JSON.stringify(jsonResponse);
-			console.log(res)
-			if(jsonResponse.status=='success'){
-				//this.setState({
-				//	bucketlists:jsonResponse.data
-				//});
-			}
-			else{
-				//alert(jsonResponse.message);
-			}
-			//this.setState({spinner:'hide'})
-		})
-
+		if (search_term.length>0){
+			fetch("https://bucketapi.herokuapp.com/api/v1/bucketlists/?search="+search_term,
+				   {headers:{
+				   		Authorization:sessionStorage.getItem('auth')
+				   },
+				   "method":"GET"}
+			).then((response)=>response.json())
+			.then((jsonResponse)=>{
+				if(jsonResponse.status=='success'){
+					this.setState({
+						results:jsonResponse.data
+					});
+				}
+				else{
+					this.setState({
+						results:[]
+					});
+				}
+				//this.setState({spinner:'hide'})
+			})
+		}
 		//console.log(searchTerm)
 	}
 
 	render(){
 		return(
-			<div className="searchbar card">
-            		<input placeholder='Search' onChange={this.search} id="search"/><i className="search-icon material-icons">search</i>
+			<div className="searchbar">
+            		<input placeholder='Search' onChange={this.search} id="search"/>
+            		<SearchResults results={this.state.results}/>
           	</div>
 			)
 	}
