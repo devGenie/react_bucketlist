7fce2bd8f7fc86b4ca85333c3aa284debd86c7f5
devGenie <jonen54@gmail.com>
2017-09-19 10:37:09 +0300

    Convert to stateless component

--- src/components/topnav.jsx
+++ src/components/topnav.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import Search from './search';
 
-class Topnav extends React.Component{
-	render(){
-		return(
-				<div className="navbar-fixed">
-						<nav>
-							<div className="nav-wrapper">
-								<div className="container">
-									<a href="#" className="brand-logo">Bucketlist Fairy</a>
-									<ul id="topNave" className="right hide-on-med-and-down">
-										<Search viewSearch={this.props.viewSearch}/>
-									</ul>
-								</div>
-							</div>
-						</nav>
-					</div>
-			)
-	}
-}
+const Topnav = viewSearch => (
+  <div className="navbar-fixed">
+    <nav>
+      <div className="nav-wrapper">
+        <div className="container">
+          <a href="#" className="brand-logo">Bucketlist Fairy</a>
+          <ul id="topNave" className="right hide-on-med-and-down">
+            <Search viewSearch={viewSearch} />
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+    );
 export default Topnav;
