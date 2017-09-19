d26e3d00388c4e3597c3a9fe8969ebdc7325ba27
devGenie <jonen54@gmail.com>
2017-09-19 10:41:07 +0300

    Convert to stateless component

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,84 +1,81 @@
 import React from 'react';
 
-class SearchResults extends React.Component{
-	constructor(props){
-		super(props)
-		this.state={results:[],display:{}}
-		this.show=this.show.bind(this)
-	}
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: [], display: {} };
+    this.show = this.show.bind(this);
+  }
 
-	componentWillReceiveProps(results){
-		this.setState({
-			results:results.results
-		})
-	}
+  componentWillReceiveProps(results) {
+    this.setState({
+      results: results.results,
+    });
+  }
 
-	show(result){
-		//alert(JSON.stringify(result))
-		this.setState({display:result})
-		this.props.viewSearch(result)
-		window.$('#view-result').modal('open');
-	}
+  show(result) {
+		// alert(JSON.stringify(result))
+    this.setState({ display: result });
+    this.props.viewSearch(result);
+    window.$('#view-result').modal('open');
+  }
 
-	render(){
-		return(
-				<div className='search-results'>
-					{this.state.results.map((result,index)=>{
-						return(
-								<div className='search-result' onClick={()=>this.show(result)}>
-									{result.name}
-								</div>
-							)
-						}
+  render() {
+    return (
+      <div className="search-results">
+        {this.state.results.map((result, index) => (
+          <div className="search-result" onClick={() => this.show(result)}>
+            {result.name}
+          </div>
+							),
 					)}
-				</div>
+      </div>
 
-			)
-	}
+    );
+  }
 }
 
-class Search extends React.Component{
-	constructor(props){
-		super(props)
-		this.state={search_term:'',results:[]}
-		this.search=this.search.bind(this)
-	}
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search_term: '', results: [] };
+    this.search = this.search.bind(this);
+  }
 
-	search(event){
-		event.preventDefault()
-		let search_term=event.target.value;
-		if (search_term.length>0){
-			fetch("https://bucketapi.herokuapp.com/api/v1/bucketlists/?search="+search_term,
-				   {headers:{
-				   		Authorization:sessionStorage.getItem('auth')
+  search(event) {
+    event.preventDefault();
+    const search_term = event.target.value;
+    if (search_term.length > 0) {
+      fetch(`https://bucketapi.herokuapp.com/api/v1/bucketlists/?search=${search_term}`,
+				   { headers: {
+				   		Authorization: sessionStorage.getItem('auth'),
 				   },
-				   "method":"GET"}
-			).then((response)=>response.json())
-			.then((jsonResponse)=>{
-				if(jsonResponse.status=='success'){
-					this.setState({
-						results:jsonResponse.data
-					});
-				}
-				else{
-					this.setState({
-						results:[]
-					});
-				}
-				//this.setState({spinner:'hide'})
-			})
-		}
-		//console.log(searchTerm)
-	}
+				   method: 'GET' },
+			).then(response => response.json())
+			.then((jsonResponse) => {
+  if (jsonResponse.status == 'success') {
+    this.setState({
+      results: jsonResponse.data,
+    });
+  }				else {
+    this.setState({
+      results: [],
+    });
+  }
+				// this.setState({spinner:'hide'})
+});
+    }
+		// console.log(searchTerm)
+  }
 
-	render(){
-		return(
-			<div className="searchbar">
-            		<input placeholder='Search' onChange={this.search} id="search"/>
-            		<SearchResults results={this.state.results} viewSearch={this.props.viewSearch} />
-          	</div>
-			)
-	}
+  render() {
+    return (
+      <div className="searchbar">
+        <input placeholder="Search" onChange={this.search} id="search" />
+        <SearchResults results={this.state.results} viewSearch={this.props.viewSearch} />
+      </div>
+    );
+  }
 }
 
 export default Search;
