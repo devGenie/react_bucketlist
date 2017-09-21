ec39638cbccd01b23b05fdc0d060ae5f4bd0d0f5
devGenie <jonen54@gmail.com>
2017-09-21 19:08:36 +0300

    Add shadows

--- src/components/topnav.jsx
+++ src/components/topnav.jsx
@@ -4,12 +4,14 @@ import Search from './search';
 const Topnav = ({searchResults}) => (
   <div className="navbar-fixed">
     <nav>
-      <div className="nav-wrapper">
+      <div className="nav-wrapper z-depth-5">
         <div className="container">
-          <a href="#" className="brand-logo">Bucketlist Fairy</a>
-          <ul id="topNave" className="right hide-on-med-and-down">
-            <Search searchResults={searchResults} />
-          </ul>
+          <div className="row">
+            <div className="col s3 hide-on-med-and-down"><a href="#" className="brand-logo">Bucketlist Fairy</a></div>
+            <div id="topNave" className="col s12 l6">
+              <Search searchResults={searchResults} />
+            </div>
+          </div>
         </div>
       </div>
     </nav>
