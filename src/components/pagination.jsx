f4e277e1501d3776c807e8b4702fdb79dafc65e2
devGenie <jonen54@gmail.com>
2017-09-22 12:37:30 +0300

    Add key to iterator

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -5,7 +5,7 @@ const Pagination = (props) =>{
            <ul className="page-container pagianation">
                 <li className="disabled page"><a href="#!"><i className="material-icons">chevron_left</i></a></li>
                     { Array.apply(null,Array(props.pages)).map((item,i)=>{
-                        return <li className="page disabled" onClick={event=>props.loadPage(i+1)}>{i+1}</li>
+                        return <li className="page disabled" key={i} onClick={event=>props.loadPage(i+1)}>{i+1}</li>
                         })
                     }
                 <li className="disabled page"><a href="#!"><i className="material-icons">chevron_right</i></a></li>
