1ff791b842f5e5fae804ba099a4f54c1c62ff649
Onen Julius <jonen54@gmail.com>
2017-09-27 19:10:10 +0300

    Remove search results

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,34 +1,5 @@
 import React from 'react';
 
-class SearchResults extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { results: [], display: {} };
-    this.show = this.show.bind(this);
-  }
-
-  show(result) {
-	// alert(JSON.stringify(result))
-    this.setState({ display: result });
-    this.props.viewSearch(result);
-    window.$('#view-result').modal('open');
-  }
-
-  render() {
-    return (
-      <div className="search-results">
-        {this.state.results.map((result, index) => (
-          <div className="search-result" onClick={() => this.show(result)}>
-            {result.name}
-          </div>
-		),
-		)}
-      </div>
-
-    );
-  }
-}
-
 class Search extends React.Component {
   constructor(props) {
     super(props);
