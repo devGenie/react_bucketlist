d5deaa9508673b31198d443e08a13ab8cdd6f97c
josiah nyarega <josiah.mokobo@andela.com>
2017-09-27 18:37:06 +0300

    add test for render

--- src/test/dashboard.test.js
+++ src/test/dashboard.test.js
@@ -4,28 +4,39 @@ import { stub, spy } from 'sinon';
 import toJson from 'enzyme-to-json';
 import 'should';
 import 'should-enzyme';
-import fetchMock from 'fetch-mock';
 import localStorageMock from '../__mocks__/localStorage';
 import DashBoard from '../components/dashboard';
 import bucketlistmock from '../__mocks__/bucketlists-mock';
 import Bucketlists from '../components/bucketlists';
+import Bucketlist from '../components/bucketlist'
 
 window.sessionStorage = localStorageMock;
 
 describe('<DashBoard>', () => {
+  let mountStab;
+  beforeEach(()=>{
+    mountStab=stub(DashBoard.prototype, 'componentDidMount').returns(true);
+  });
+
+  afterEach(()=>{
+    mountStab.restore()
+  })
+
   it('calls componentDidMount', () => {
-      spy(DashBoard.prototype, 'componentDidMount');
+      //stub(DashBoard.prototype, 'componentDidMount').returns(true);
       const wrapper=mount(<DashBoard />);
       expect(DashBoard.prototype.componentDidMount.calledOnce).toBe(true);
     });
 
-    it('renders bucketlists',()=>{
-      const wrapper=mount(<Bucketlists data={bucketlistmock['data']} />)
-      expect(wrapper.find('.bucketlist')).toHaveLength(4);
-    })
-
     it('renders correctly', ()=>{
+      //stub(DashBoard.prototype, 'componentDidMount').returns(true);
         const wrapper=shallow(<DashBoard/>);
         expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders bucketlists',()=>{
+      stub(Bucketlist.prototype, 'componentDidMount').returns(true);
+      const wrapper=mount(<Bucketlists data={bucketlistmock['data']} />)
+      expect(wrapper.find('.bucketlist')).toHaveLength(4);
     })
 });
