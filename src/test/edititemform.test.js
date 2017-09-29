a62f956fe3a660449f1261de052dcb77f264a296
Onen Julius <jonen54@gmail.com>
2017-09-29 10:59:26 +0300

    Add snapshot test

---
+++ src/test/edititemform.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount } from 'enzyme';
+import {stub, spy} from 'sinon';
+import sinon from 'sinon';
+import 'should';
+import 'should-enzyme';
+import toJson from 'enzyme-to-json';
+import sinonStubPromise from 'sinon-stub-promise';
+
+import EditItem from '../components/edit-item';
+import DashBoard from '../components/dashboard';
+import localStorageMock from '../__mocks__/localStorage';
+
+sinonStubPromise(sinon)
+window.sessionStorage = localStorageMock;
+
+describe('<BucketlistForm/>',()=>{
+	let formMount;
+	let fetchMock;
+	let changeSpy;
+	let submitSpy;
+	let caller;
+
+	beforeEach(()=>{
+		fetchMock = stub(window,'fetch').returnsPromise().resolves({status:'success'});
+		caller = spy(DashBoard.prototype,'handleFetchCaller');
+		changeSpy = spy(EditItem.prototype,'handleChange');
+		submitSpy = spy(EditItem.prototype,'handleSubmit');
+		formMount = mount(<EditItem caller={{bucketlist:'1',item:{id:'1',name:'one'},callback:''}}/>);
+	});
+
+	afterEach(()=>{
+		fetchMock.restore();
+		changeSpy.restore();
+		submitSpy.restore();
+		caller.restore();
+	});
+
+	it('can render correctly',()=>{
+		expect(toJson(formMount)).toMatchSnapshot()
+	});
+
+	
+})
