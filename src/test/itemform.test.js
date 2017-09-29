45a11e6ac0ccfe1d9d7e349573cd12383ff3ddc4
Onen Julius <jonen54@gmail.com>
2017-09-29 10:51:00 +0300

    Add test for item form

---
+++ src/test/itemform.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount } from 'enzyme';
+import {stub, spy} from 'sinon';
+import sinon from 'sinon';
+import 'should';
+import 'should-enzyme';
+import toJson from 'enzyme-to-json';
+import sinonStubPromise from 'sinon-stub-promise';
+
+import ItemForm from '../components/item-form';
+import DashBoard from '../components/dashboard';
+import localStorageMock from '../__mocks__/localStorage';
+
+sinonStubPromise(sinon)
+window.sessionStorage = localStorageMock;
+
+describe('<ItemForm/>',()=>{
+	let formMount;
+	let fetchMock;
+	let changeSpy;
+	let submitSpy;
+	let caller;
+
+	beforeEach(()=>{
+		fetchMock = stub(window,'fetch').returnsPromise().resolves({status:'success'});
+		caller = spy(DashBoard.prototype,'handleFetchCaller');
+		changeSpy = spy(ItemForm.prototype,'handleChange');
+		submitSpy = spy(ItemForm.prototype,'handleSubmit');
+		formMount = mount(<ItemForm caller={caller}/>);
+	});
+
+	afterEach(()=>{
+		fetchMock.restore();
+		changeSpy.restore();
+		submitSpy.restore();
+		caller.restore();
+	});
+
+	it('can render correctly',()=>{
+		expect(toJson(formMount)).toMatchSnapshot()
+	});
+
+	it('can submit data',()=>{
+		formMount.find('form').simulate('submit',{
+			preventDefault:()=>{}
+		});
+		expect(submitSpy.called).toBe(true)
+	});
+
+	it('can handle change event',()=>{
+		formMount.find('form').find({name:'name'}).simulate('change',{
+			target:{
+				value:'changed'
+			}
+		});
+		expect(changeSpy.called).toBe(true)
+	})
+})
