a1a9a10aecad949e39613b8eb429a4b8430fd9ac
Onen Julius <jonen54@gmail.com>
2017-10-02 12:07:51 +0300

    Add logout callback

--- src/components/user-data.jsx
+++ src/components/user-data.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const User= username=>{
+const User= ({logout})=>{
     return(
            <div className="col s12" id="dropdown-btn">
-                <a id="navbar-drop" className='dropdown-button col s12' data-activates='user-dropdown'>Onen Julius</a>
+                <a id="navbar-drop" className='dropdown-button col s12' data-activates='user-dropdown'>Account</a>
                   <ul id='user-dropdown' className='dropdown-content'>
-                    <li><a href="#!">Change password</a></li>
+                    <li data-target="password-modal"><a href="#!">Change password</a></li>
                     <li className="divider"></li>
-                    <li><a href="#!"><i className="material-icons">view_module</i>Sign out</a></li>
+                    <li onClick={logout}><a href="#!"><i className="material-icons">view_module</i>Sign out</a></li>
                   </ul>
            </div>
         )
