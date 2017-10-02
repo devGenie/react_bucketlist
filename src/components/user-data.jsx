e00428bb257224d915f3fc881ba19b34f91201d9
Onen Julius <jonen54@gmail.com>
2017-10-02 15:03:00 +0300

    Change account to icon

--- src/components/user-data.jsx
+++ src/components/user-data.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const User= ({logout})=>{
     return(
            <div className="col s12" id="dropdown-btn">
-                <a id="navbar-drop" className='dropdown-button col s12' data-activates='user-dropdown'>Account</a>
+                <a id="navbar-drop" className='dropdown-button col s12' data-activates='user-dropdown'>
+                <i className="large material-icons">account_circle</i></a>
                   <ul id='user-dropdown' className='dropdown-content'>
                     <li data-target="password-modal"><a href="#!">Change password</a></li>
                     <li className="divider"></li>
